879dcf82c84ca3cfcac022f732659afaec35028b
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-12-20 08:19:49 -0300

    Removed unnecessary code. "action-types" were created to avoid problems with strings.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducer';
 
-// Configuración de Redux DevTools Extension
 const composeEnhancers =
-  (typeof window !== 'undefined' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+    (typeof window !== 'undefined' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-// Creación de la tienda sin Redux Thunk
 const store = createStore(
-  rootReducer,
-  composeEnhancers(/* Aquí puedes agregar más middlewares si es necesario */)
+    rootReducer,
+    composeEnhancers()
 );
 
 export default store;
