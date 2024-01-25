deaa45b36ac5cd3d3e60fd88bacbe8d16b2c57a7
jackl92 <71673745+jackl92@users.noreply.github.com>
2024-01-25 01:21:15 +0000

    Refactor keyboard language and style selectors

--- src/Components/KeyBoardLanguage.jsx
+++ src/Components/KeyBoardLanguage.jsx
@@ -9,23 +9,25 @@ function KeyBoardLanguage(props) {
   const setLanguage = props.setLanguage;
   const handleChange = (event) =>  setLanguage(event.target.value);
   return (
-    <div className="language-and-icons">
-      <select className="chooseLanguage" onChange={handleChange}>
-        {
-          languagesData.map((languege,index)=>
-          <option key ={index}value={languege.languageName}>{languege.translatedName}</option>
-          )
-        }
+    <div className='language-and-icons'>
+      <select className='chooseLanguage' onChange={handleChange}>
+        {languagesData.map((languege, index) => (
+          <option key={index} value={languege.languageName}>
+            {languege.translatedName}
+          </option>
+        ))}
       </select>
       {props.isEmojiActive ? (
-        <FaKeyboard
-          className="icon"
-          id="keyboard-icon"
-          onClick={props.changeState}
-        />
-      ) : (
+
         <button className="icon" onClick={props.changeState}>
-          <img src={emojiButton} alt="Emoji Keyboard" />
+          <FaKeyboard 
+            size={50}
+            alt='virtual Keyboard'
+          />
+        </button>
+      ) : (
+        <button className='icon' onClick={props.changeState}>
+          <img src={emojiButton} alt='Emoji Keyboard' />
         </button>
       )}
     </div>
--- src/Components/StyleSelector.jsx
+++ src/Components/StyleSelector.jsx
@@ -64,86 +64,108 @@ function StyleSelector(props) {
     changeStyle(styleToChange);
   };
   return (
-    <div className="d-flex flex-column style-selector-container">
-      <div className="d-flex align-items-center justify-center row style-selector" style={{marginTop: 0}}>
-        <button onClick={lowerAll} className="size-xl">LOWER ALL</button>
-        <button onClick={upperAll} className="size-xl">UPPER ALL</button>
+    <div className='d-flex flex-column style-selector-container'>
+      <div
+        className='d-flex align-items-center justify-center row style-selector'
+        style={{ marginTop: 0 }}
+      >
+        <button onClick={lowerAll} className='size-xl'>
+          LOWER ALL
+        </button>
+        <button onClick={upperAll} className='size-xl'>
+          UPPER ALL
+        </button>
         <button
           onClick={handleTextDecorationChange}
-          className={`size-xl ${ currentStyle.textDecoration === "underline" ? "active" : ""}`}
+          className={`size-xl ${
+            currentStyle.textDecoration === 'underline' ? 'active' : ''
+          }`}
         >
           <FaUnderline />
         </button>
       </div>
 
-      <div className="row style-selector d-flex flex-row justify-center" style={{ alignItems: "end" }}>
+      <div
+        className='row style-selector d-flex flex-row justify-center'
+        style={{ alignItems: 'end' }}
+      >
         <div>
           <label>Font Size:</label>
-          <div className="d-flex" style={{ alignItems: "flex-end" }}>
+          <div className='d-flex' style={{ alignItems: 'flex-end' }}>
             <button
               onClick={() => handleFontSizeChange(-1)}
-              style={{ width: "22px", marginBottom: "0px" }}
+              style={{ width: '22px', marginBottom: '0px' }}
             >
               -
             </button>
             <input
-              type="text"
+              type='text'
               value={currentStyle.fontSize}
-              onChange={() => { }}
-              style={{ marginLeft: "5px", marginRight: "5px", width: "40px" }}
+              onChange={() => {}}
+              style={{ marginLeft: '5px', marginRight: '5px', width: '40px' }}
             />
             <button
               onClick={() => handleFontSizeChange(+1)}
-              style={{ width: "22px", marginBottom: "0px" }}
+              style={{ width: '22px', marginBottom: '0px' }}
             >
               +
             </button>
           </div>
         </div>
 
-        <div className="d-flex flex-column align-items-center">
+        <div className='d-flex flex-column align-items-center'>
           <label>Font Family:</label>
           <select
             value={currentStyle.fontFamily}
-            name="fontFamily"
+            name='fontFamily'
             onChange={handleChangeStyle}
           >
-            <FontOption fontFamily="Arial">Arial</FontOption>
-            <FontOption fontFamily="Times New Roman">Times New Roman</FontOption>
-            <FontOption fontFamily="Verdana">Verdana</FontOption>
-            <FontOption fontFamily="Courier New">Courier New</FontOption>
-            <FontOption fontFamily="Georgia">Georgia</FontOption>
-            <FontOption fontFamily="Impact">Impact</FontOption>
-            <FontOption fontFamily="Comic Sans MS">Comic Sans MS</FontOption>
-            <FontOption fontFamily="Trebuchet MS">Trebuchet MS</FontOption>
-            <FontOption fontFamily="Palatino Linotype">Palatino Linotype</FontOption>
-            <FontOption fontFamily="Lucida Console">Lucida Console</FontOption>
+            <FontOption fontFamily='Arial'>Arial</FontOption>
+            <FontOption fontFamily='Times New Roman'>
+              Times New Roman
+            </FontOption>
+            <FontOption fontFamily='Verdana'>Verdana</FontOption>
+            <FontOption fontFamily='Courier New'>Courier New</FontOption>
+            <FontOption fontFamily='Georgia'>Georgia</FontOption>
+            <FontOption fontFamily='Impact'>Impact</FontOption>
+            <FontOption fontFamily='Comic Sans MS'>Comic Sans MS</FontOption>
+            <FontOption fontFamily='Trebuchet MS'>Trebuchet MS</FontOption>
+            <FontOption fontFamily='Palatino Linotype'>
+              Palatino Linotype
+            </FontOption>
+            <FontOption fontFamily='Lucida Console'>Lucida Console</FontOption>
           </select>
         </div>
 
-        <div className="d-flex flex-column align-items-center">
-          <label style={{ whiteSpace: "nowrap" }}>Text-Align:</label>
+        <div className='d-flex flex-column align-items-center'>
+          <label style={{ whiteSpace: 'nowrap' }}>Text-Align:</label>
           <select
             value={currentStyle.textAlign}
             onChange={(e) => handleTextAlignmentChange(e.target.value)}
           >
-            <option value="center">Center</option>
-            <option value="left">Left</option>
-            <option value="right">Right</option>
+            <option value='center'>Center</option>
+            <option value='left'>Left</option>
+            <option value='right'>Right</option>
           </select>
         </div>
-        
+
         <div>
           <label>Color:</label>
-          <input type="color" id="colorInput" value={currentStyle.color} name="color" onChange={handleChangeStyle} />
+          <input
+            type='color'
+            id='colorInput'
+            value={currentStyle.color}
+            name='color'
+            onChange={handleChangeStyle}
+          />
         </div>
       </div>
 
-      <div className="row style-selector">
+      <div className='row style-selector'>
         <button
           onClick={handleToggleChangeAllStyle}
-          className={`${changeAllStyle ? "active" : ""}`}
-          style={{padding: "8px 16px"}}
+          className={`${changeAllStyle ? 'active' : ''}`}
+          style={{ padding: '8px 16px' }}
         >
           change all text's style
         </button>
